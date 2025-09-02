e6e448d2f0d1429311dedb1fc967ba1018f9e630
@Akih_san <112370246+akih-san@users.noreply.github.com>
2025-09-02 17:34:06 +0900

    Add files via upload

--- i86src/msdos/mkiosys.bat
+++ i86src/msdos/mkiosys.bat
@@ -1,3 +1,7 @@
 asl -L MEZ86IO.ASM
 p2bin MEZ86IO.p
-powershell ./mkiosys.ps1
+powershell .\mkiosys.ps1
+copy MSDOS.SYS ..\msdos_bin\.
+copy IO.SYS ..\msdos_bin\.
+copy MSDOS.SYS ..\..\DISKS\DOSDISKS\.
+copy IO.SYS ..\..\DISKS\DOSDISKS\.
